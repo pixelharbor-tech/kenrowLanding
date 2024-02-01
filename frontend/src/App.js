ebaa80f6a885092c41a26ea51c5ea55884c1b35c
pixelharbor-tech <pixelharbor0@gmail.com>
2024-02-01 07:13:08 +0100

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import { useEffect } from 'react';
-import {Route, Routes, useNavigate } from "react-router-dom"
-import Home from "./pages/Home"
-import WhoWeHelp from "./pages/Who-we-help"
-import SocialMediaAds from './pages/TiktokAds';
-import SEO from './pages/SEO';
-import Blog from './pages/Blog';
-import GoogleAds from './pages/GoogleAds';
-import FacebookAds from './pages/FacebookAds';
-import Content from './pages/Content';
-import EmailMarketing from './pages/EmailMarketing';
-import SocialPosting from './pages/SocialPosting';
-import Website from './pages/Website';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import FreeConsultation from './pages/FreeConsultation';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CaseStudies from './pages/CaseStudies';
-import Listings from './pages/Listings';
+import { Route, Routes, useNavigate } from "react-router-dom"
+import Home from "@pages/Home"
+import WhoWeHelp from "@pages/Who-we-help"
+import SocialMediaAds from '@pages/TiktokAds';
+import SEO from '@pages/SEO';
+import Blog from '@pages/Blog';
+import GoogleAds from '@pages/GoogleAds';
+import FacebookAds from '@pages/FacebookAds';
+import Content from '@pages/Content';
+import EmailMarketing from '@pages/EmailMarketing';
+import SocialPosting from '@pages/SocialPosting';
+import Website from '@pages/Website';
+import About from '@pages/About';
+import Contact from '@pages/Contact';
+import FreeConsultation from '@pages/FreeConsultation';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import CaseStudies from '@pages/CaseStudies';
+import Listings from '@pages/Listings';
 function App() {
   const navigate = useNavigate();
 
@@ -29,9 +29,9 @@ function App() {
     }
   }, [navigate])
   return (
-       <>
-       <Header />
-       <Routes>
+    <>
+      <Header />
+      <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/who-we-help' element={<WhoWeHelp />} />
         <Route path='/seo' element={<SEO />} />
@@ -47,13 +47,13 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/book-consultation' element={<FreeConsultation />} />
 
-        <Route path='/listing' element={<Listings/>} />
+        <Route path='/listing' element={<Listings />} />
         <Route path='/case-studies' element={<CaseStudies />} />
       </Routes>
       <Footer />
-       </>
-      
-    
+    </>
+
+
   );
 }
 
