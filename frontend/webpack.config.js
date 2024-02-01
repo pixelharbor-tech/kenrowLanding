b07de8a35208571d5468c1be05f8526dc368a619
pixelharbor-tech <pixelharbor0@gmail.com>
2024-02-01 06:52:48 +0100

    done

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
     module: {
         rules: [
@@ -15,7 +17,30 @@ module.exports = {
             {
                 test: /\.svg$/,
                 use: ['@svgr/webpack']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                 mode: 'development'
             }
-        ]
-    }
+            
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@components': path.resolve(__dirname, 'src/components'),
+        },
+    },
 };
