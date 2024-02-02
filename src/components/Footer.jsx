3b925a852b2f59f1a3692f50853e58214df82edf
pixelharbor-tech <pixelharbor0@gmail.com>
2024-02-02 01:11:22 +0100

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
             </p>
 
             <span>
-              &copy; Copyright Kenrow Digital Marketing Agency. All Rights
+              &copy; Copyright Kenrow Digital Marketing Agency 90. All Rights
               Reserved
             </span>
           </div>
